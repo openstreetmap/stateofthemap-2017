49662233e6e10e6c30548929eb61c06d1d8fe370
RobJN <rob.j.nickerson@gmail.com>
2017-03-17 01:15:50 +0000

    Revert "Option 1, Automatic switch by language, using javascript."

--- js/lang.js
+++
@@ -1,8 +0,0 @@
-var language = (window.navigator.userLanguage || window.navigator.language || window.navigator.browserLanguage).substr(0,2);
-
-if (language == "ja" ) {
-   var uri = window.location.href;
-   if ( !uri.match(/ja/) ) {
-      window.location.href = './ja/'
-   }
-}
